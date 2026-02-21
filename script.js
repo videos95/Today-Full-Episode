d025818ce3715c49f15f1239b5a5590ef24d155e
videos95 <demerriya54@gmail.com>
2026-02-21 09:14:47 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+let viewers = Math.floor(Math.random()*40000)+40000;
+setInterval(()=>{
+  viewers += Math.floor(Math.random()*300-150);
+  if(viewers<1000) viewers=1000;
+  document.getElementById("viewers").innerText = viewers.toLocaleString();
